049c00aea861d25b674ccd1f0a5d0ac5b5073ca4
Sarah <sarah@sekai.co.uk>
2019-12-01 12:12:24 +0000

    feat(footer): add ga tracking to outbound link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      &copy; Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
+      &copy; 2019 Built with{' '}
+      <OutboundLink href="https://www.gatsbyjs.org/">Gatsby.js</OutboundLink>
     </p>
   </footer>
 )
