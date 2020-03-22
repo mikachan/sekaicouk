b6108766b1a7d9b0c32b7e65c96eb963ba9f72e6
Sarah <sarah@sekai.co.uk>
2020-03-22 16:22:12 +0000

    chore: remove work and about from menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = props => (
+const Header = (props) => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
       <span className="icon fa-heart"></span>
@@ -27,7 +27,7 @@ const Header = props => (
             Intro
           </a>
         </li>
-        <li>
+        {/* <li>
           <a
             href="javascript:;"
             onClick={() => {
@@ -46,7 +46,7 @@ const Header = props => (
           >
             About
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             href="javascript:;"
