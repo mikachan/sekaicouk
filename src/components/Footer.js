981775e134911e53cae1ec7083a815f86ad1f943
Sarah <sarah@sekai.co.uk>
2020-03-24 19:31:24 +0000

    chore: add current year to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,22 @@
-import { OutboundLink } from 'gatsby-plugin-google-analytics'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-const Footer = props => (
-  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">
-      &copy; 2019 Built with{' '}
-      <OutboundLink href="https://www.gatsbyjs.org/">Gatsby.js</OutboundLink>
-    </p>
-  </footer>
-)
+const currentDate = new Date();
+
+const Footer = (props) => (
+	<footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+		<p className="copyright">
+			&copy; {currentDate.getFullYear()} Built with{' '}
+			<OutboundLink href="https://www.gatsbyjs.org/">
+				Gatsby.js
+			</OutboundLink>
+		</p>
+	</footer>
+);
 
 Footer.propTypes = {
-  timeout: PropTypes.bool,
-}
+	timeout: PropTypes.bool,
+};
 
-export default Footer
+export default Footer;
