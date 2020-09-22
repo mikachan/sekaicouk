84a468a681327e627fa7068dd91a39c12e569de4
Sarah <sarah@sekai.co.uk>
2020-09-22 22:09:20 +0100

    fix: path to code content

--- src/routes/code/index.json.js
+++ src/routes/code/index.json.js
@@ -3,9 +3,9 @@ import grayMatter from 'gray-matter';
 import path from 'path';
 
 const getAllPosts = () =>
-	fs.readdirSync('src/content/photography').map((fileName) => {
+	fs.readdirSync('src/content/code').map((fileName) => {
 		const post = fs.readFileSync(
-			path.resolve('src/content/photography', fileName),
+			path.resolve('src/content/code', fileName),
 			'utf-8'
 		);
 		return grayMatter(post).data;
