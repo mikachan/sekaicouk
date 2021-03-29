35ef053daacdd412068b8afebc1b8f9dc4f53749
Sarah Norris <sarah@sekai.co.uk>
2021-03-29 11:47:22 +0100

    chore: add more info to about section

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,9 +33,59 @@ function Main(props) {
 					{/* <img src={pic01} alt="" /> */}
 				</span>
 				<p>
-					Web developer from Lancashire. Loves music, peanut butter,
-					photography, traveling, video games, dragons &amp; animals.
+					Sarah is a relatively geeky girl who lives in{' '}
+					<OutboundLink
+						href="https://www.visitlancashire.com/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Lancashire, UK
+					</OutboundLink>{' '}
+					with her dog, Thora.
 				</p>
+
+				<p>
+					She is a self-taught web developer with a background in
+					marketing and statistics. She's been building websites since
+					around 2000 and has been paid for doing it since 2008. She
+					currently specialises in front end development, including
+					React and Svelte.
+				</p>
+
+				<p>
+					In her spare time she enjoys walking, photography,
+					traveling, occasional bass guitar and video games. She also
+					volunteers at{' '}
+					<OutboundLink
+						href="https://www.parbolddouglasmusic.org/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Parbold Douglas Music
+					</OutboundLink>{' '}
+					and{' '}
+					<OutboundLink
+						href="https://www.wigansteam.co.uk/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Wigan STEAM code club
+					</OutboundLink>
+					.
+				</p>
+
+				<p>
+					Some of her favourite things are peanut butter, dragons,
+					animals and pretty much anything Japanese, like{' '}
+					<OutboundLink
+						href="https://you.sekai.co.uk"
+						target="_blank"
+					>
+						this guitarist
+					</OutboundLink>
+					.
+				</p>
+
 				{close}
 			</article>
 
@@ -48,6 +98,16 @@ function Main(props) {
 			>
 				<h2 className="major">Contact</h2>
 				<ul className="icons">
+					<li>
+						<OutboundLink
+							href="https://github.com/mikachan"
+							target="_blank"
+							rel="noreferrer"
+							className="icon fa-github"
+						>
+							<span className="label">GitHub</span>
+						</OutboundLink>
+					</li>
 					<li>
 						<OutboundLink
 							href="https://www.linkedin.com/in/sarahnorris88"
@@ -70,6 +130,16 @@ function Main(props) {
 					</li>
 					<li>
 						<OutboundLink
+							href="https://unsplash.com/@mikachan_"
+							target="_blank"
+							rel="noreferrer"
+							className="icon fa-camera"
+						>
+							<span className="label">Unsplash</span>
+						</OutboundLink>
+					</li>
+					<li>
+						<OutboundLink
 							href="https://last.fm/user/mikachan_"
 							target="_blank"
 							rel="noreferrer"
@@ -86,16 +156,6 @@ function Main(props) {
 							className="icon fa-instagram"
 						>
 							<span className="label">Instagram</span>
-						</OutboundLink>
-					</li>
-					<li>
-						<OutboundLink
-							href="https://github.com/mikachan"
-							target="_blank"
-							rel="noreferrer"
-							className="icon fa-github"
-						>
-							<span className="label">Github</span>
 						</OutboundLink>
 					</li>
 				</ul>
