086f2d5ec9493ef3294164ff9f0900eb5ef570a2
Sarah Norris <sarah@sekai.co.uk>
2023-01-01 22:33:10 +0000

    chore: update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer = (props) => (
 	<footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-		<p className="copyright">&copy; 2003-2022.</p>
+		<p className="copyright">&copy; 2003-2023.</p>
 	</footer>
 );
 
