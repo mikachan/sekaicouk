8708175da200c90eea787582998dd420c974ac10
Sarah Norris <sarah@sekai.co.uk>
2026-05-15 20:06:42 +0100

    chore: add social metadata tags

--- src/components/SiteHead.js
+++ src/components/SiteHead.js
@@ -16,6 +16,13 @@ const SiteHead = ({
 			<title>{pageTitle}</title>
 			<meta name="description" content={description} />
 			<meta name="keywords" content={keywords} />
+			<meta property="og:title" content={pageTitle} />
+			<meta property="og:description" content={description} />
+			<meta property="og:type" content="website" />
+			<meta property="og:site_name" content={siteTitle} />
+			<meta name="twitter:card" content="summary" />
+			<meta name="twitter:title" content={pageTitle} />
+			<meta name="twitter:description" content={description} />
 		</>
 	);
 };
